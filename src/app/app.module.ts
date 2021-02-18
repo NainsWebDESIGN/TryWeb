0e66790354949a067668e8275e3be69a08322023
Nains <jason20226@gmail.com>
2021-02-18 17:59:45 +0800

    2021-02-18-development

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,16 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { DevelopmentComponent } from './development/development.component';
 
 
 @NgModule({
-  declarations: [		
+  declarations: [			
     AppComponent,
     HeaderComponent,
       HomeComponent,
-      AboutComponent
+      AboutComponent,
+      DevelopmentComponent
    ],
   imports: [
     BrowserModule
