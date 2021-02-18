9fa092c8fd49f2e247a2a167271df15d2329dda8
Nains <jason20226@gmail.com>
2021-02-18 16:42:31 +0800

    2021-02-18-新增home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
-  declarations: [		
+  declarations: [	
     AppComponent,
-      HeaderComponent,
-      FooterComponent
+    HeaderComponent,
+      HomeComponent
    ],
   imports: [
     BrowserModule
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app_header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  menu: boolean = false;
+  Sticky: boolean = false;
+  @HostListener('window:scroll', ['$event'])
+  Scroll(_Event: any) {
+    //客戶端高度
+    // let clientH = _Event.path[0].scrollingElement.clientHeight;
+    //body高度
+    // let bodyH = _Event.path[0].scrollingElement.clientHeight;
+    //滾動的高度
+    let scrollTop = _Event.path[0].scrollingElement.scrollTop;
+    this.Sticky = (scrollTop > 0) ? true : false;
+  }
   constructor() { }
-
+  MenuEffects() {
+    this.menu = !this.menu;
+  }
   ngOnInit() {
   }
 
