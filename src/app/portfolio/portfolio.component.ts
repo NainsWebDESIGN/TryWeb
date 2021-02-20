0e36740f9fa85546ba212dd7a5726a95672081ee
Nains <jason20226@gmail.com>
2021-02-20 14:26:53 +0800

    2021-02-20-filter完成

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -6,9 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./portfolio.component.scss']
 })
 export class PortfolioComponent implements OnInit {
-
+  header: string = '*';
   constructor() { }
-
+  check(_Name: string) {
+    this.header = _Name;
+    switch (_Name) {
+      case '*':
+        $('.grid-item').fadeIn('300');
+        break;
+      default:
+        $('.grid-item').not('.' + _Name).fadeOut('300');
+        $('.grid-item').filter('.' + _Name).fadeIn('300');
+        break;
+    }
+  }
   ngOnInit() {
   }
 
