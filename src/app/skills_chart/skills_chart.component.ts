b39d38634e552eb74fa6d8671aa3981db95eab30
Nains <jason20226@gmail.com>
2021-02-22 13:47:47 +0800

    2021-02-22-滾動動畫

--- src/app/skills_chart/skills_chart.component.ts
+++ src/app/skills_chart/skills_chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -7,7 +7,19 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./skills_chart.component.scss']
 })
 export class Skills_chartComponent implements OnInit {
+  @HostListener('window:scroll', ['$event'])
+  Scroll(_Event: any) {
+    //客戶端高度
+    // let clientH = _Event.path[0].scrollingElement.clientHeight;
+    //body高度
+    // let bodyH = _Event.path[0].scrollingElement.clientHeight;
+    //滾動的高度
+    let scrollTop = _Event.path[0].scrollingElement.scrollTop;
+    console.log(scrollTop);
+    this.scrollBox = scrollTop > 1585 ? true : false;
+  }
   data: any = { line: [], circle: [] };
+  scrollBox: boolean = false;
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
