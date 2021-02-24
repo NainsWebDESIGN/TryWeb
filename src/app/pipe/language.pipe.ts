1b7a72a0f95fe84cdfbf95875699b63c9704bb57
Nains <jason20226@gmail.com>
2021-02-24 17:49:06 +0800

    2021-02-24-刪除不必要之console

--- src/app/pipe/language.pipe.ts
+++ src/app/pipe/language.pipe.ts
@@ -6,7 +6,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class LanguagePipe implements PipeTransform {
 
   transform(_Value: string, _Obj: any) {
-    console.log(_Value);
     return !_Obj || !_Obj[_Value] ? _Value : _Obj[_Value];
   }
 
